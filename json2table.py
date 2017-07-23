0a8969545a24bc949d69d04282054b9aaa74c963
abreschi <ale.breschi@gmail.com>
2017-07-23 22:41:19 +0200

    process comments as well

--- json2table.py
+++ json2table.py
@@ -34,8 +34,14 @@ cols = (
   "averageweight",
 )
 
-print "\t".join(["id"] + [col for col in cols])
+out1 = open("mdata.games.tsv", "w")
+out3 = open("comments.users.tsv", "w")
+
+out1.write("\t".join(["id"] + [col for col in cols]) + "\n")
+out3.write("\t".join(("id", "rating", "username", "text")) + "\n")
+
 for k,v in d.iteritems():
+
   line = "\t".join(
     [k] +
     [
@@ -45,6 +51,22 @@ for k,v in d.iteritems():
         for col in cols
     ]
   )
-  print line
+  out1.write(line + "\n")
+
+  comments = v.get("comments", [])
+  for comment in comments:
+    out3.write("\t".join((
+      k, 
+      comment["rating"], 
+      comment["username"],
+      comment["value"].replace("\n"," ").replace("\t", " ") 
+        if comment["value"]
+        else "NA",
+    ))
+    + "\n")
+
+
+out1.close()
+out3.close()
 
 
